0838503b3580266ae43b392ca280e1439c4db822
Lufty Wiranda <lufty.wiranda@gmail.com>
2017-08-09 00:27:49 +0700

    refactor(repo): reduce boilerplate

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const pathExists = require('path-exists');
 const shell = require('shelljs');
 
-const removeLockfiles = require('../');
+const removeLockfiles = require('./');
 
 test('removes lockfiles', () => {
   const lockfiles = ['package-lock.json', 'yarn.lock'];
