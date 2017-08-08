be72b6e97602bf5168c07085245088554185253a
Lufty Wiranda <lufty.wiranda@gmail.com>
2017-08-08 23:05:34 +0700

    test: write test

---
+++ __tests__/index.js
@@ -0,0 +1,13 @@
+const pathExists = require('path-exists');
+const shell = require('shelljs');
+
+const removeLockfiles = require('../');
+
+test('removes lockfiles', () => {
+  const lockfiles = ['package-lock.json', 'yarn.lock'];
+
+  shell.touch(lockfiles);
+  removeLockfiles();
+
+  expect(pathExists.sync(lockfiles)).toBe(false);
+});
