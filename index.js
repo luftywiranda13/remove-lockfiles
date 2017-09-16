f4dd8fea8c0e9c5ed2e7a1e52bfff3992173e5ea
Lufty Wiranda <lufty.wiranda@gmail.com>
2017-09-17 05:13:48 +0700

    refactor(API): convert code to asynchronous (promise)

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 'use strict';
 
+const path = require('path');
+const del = require('del');
+const execa = require('execa');
 const hasLockfile = require('has-lockfile');
-const shell = require('shelljs');
 
-shell.config.silent = true;
+const rm = file => execa('git', ['rm', '--cached', file]);
+const unstage = file => execa('git', ['reset', 'HEAD', file]);
 
-module.exports = () => {
-  const lockfile = hasLockfile();
+module.exports = cwd => {
+  cwd = path.resolve(cwd || process.cwd());
+
+  const lockfile = hasLockfile(cwd);
 
   if (lockfile !== null) {
-    shell.exec(`git rm --cached ${lockfile}`);
-    shell.rm('-f', lockfile);
+    const target = path.join(cwd, lockfile);
 
-    return lockfile;
+    rm(target).catch(() => {
+      unstage(target);
+    });
+
+    return del(lockfile, { cwd });
   }
-
-  return lockfile;
 };
