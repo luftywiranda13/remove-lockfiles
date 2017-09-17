b2d4090a3c6ef300d65fecdd23e8581852c7f52e
Lufty Wiranda <lufty.wiranda@gmail.com>
2017-09-17 22:28:12 +0700

    test: add tests

--- test.js
+++ test.js
@@ -5,21 +5,44 @@ const shell = require('shelljs');
 
 const removeLockfiles = require('./');
 
-test('removes package-lock.json', async () => {
-  shell.touch('package-lock.json');
-  await removeLockfiles();
+describe('CWD', () => {
+  test('removes package-lock.json', async () => {
+    shell.touch('package-lock.json');
+    await removeLockfiles();
 
-  expect(hasLockfile()).toBe(null);
+    expect(hasLockfile()).toBe(null);
+  });
+
+  test('removes yarn.lock', async () => {
+    shell.touch('yarn.lock');
+    await removeLockfiles();
+
+    expect(hasLockfile()).toBe(null);
+  });
+
+  test('do nothing', async () => {
+    await removeLockfiles();
+    expect(hasLockfile()).toBe(null);
+  });
 });
 
-test('removes yarn.lock', async () => {
-  shell.touch('yarn.lock');
-  await removeLockfiles();
+describe('outside CWD', () => {
+  test('removes package-lock.json', async () => {
+    shell.touch('../package-lock.json');
+    await removeLockfiles('../');
 
-  expect(hasLockfile()).toBe(null);
-});
+    expect(hasLockfile('../')).toBe(null);
+  });
 
-test('do nothing', async () => {
-  await removeLockfiles();
-  expect(hasLockfile()).toBe(null);
+  test('removes yarn.lock', async () => {
+    shell.touch('../yarn.lock');
+    await removeLockfiles('../');
+
+    expect(hasLockfile('../')).toBe(null);
+  });
+
+  test('do nothing', async () => {
+    await removeLockfiles('../');
+    expect(hasLockfile('../')).toBe(null);
+  });
 });
