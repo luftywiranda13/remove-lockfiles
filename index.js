9c4fc2f07734a4474b72e2d829ef849265edcc4b
Lufty Wiranda <lufty.wiranda@gmail.com>
2017-09-17 22:33:17 +0700

    feat(API): run process asynchronously (Promise) (#34)

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const path = require('path');
 const del = require('del');
 const execa = require('execa');
 const hasLockfile = require('has-lockfile');
@@ -11,16 +10,10 @@ module.exports = cwd => {
   const lockfile = hasLockfile(cwd);
 
   if (lockfile !== null) {
-    const target = path.join(cwd, lockfile);
-
-    try {
-      execa.sync('git', ['rm', '--cached', target]);
-    } catch (err) {
-      execa('git', ['reset', 'HEAD', target]);
-    } finally {
-      del.sync(target, {force: true, cwd});
-    }
+    return execa('git', ['rm', '-f', lockfile], {cwd, reject: false})
+      .then(() => del(lockfile, {cwd}))
+      .then(() => lockfile);
   }
 
-  return lockfile;
+  return Promise.resolve(lockfile);
 };
