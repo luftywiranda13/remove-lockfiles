2d3a405f05596eceab54244e1a8eb477caa11379
Lufty Wiranda <lufty.wiranda@gmail.com>
2017-09-21 15:26:15 +0700

    test: reorganize tests

--- test.js
+++ test.js
@@ -8,67 +8,71 @@ const removeLockfiles = require('./');
 describe('CWD', () => {
   it('removes package-lock.json', async () => {
     shell.touch('package-lock.json');
+
     const res = await removeLockfiles();
 
-    expect(hasLockfile()).toEqual([]);
     expect(res).toEqual(['package-lock.json']);
+    expect(hasLockfile()).toEqual([]);
   });
 
   it('removes yarn.lock', async () => {
     shell.touch('yarn.lock');
+
     const res = await removeLockfiles();
 
-    expect(hasLockfile()).toEqual([]);
     expect(res).toEqual(['yarn.lock']);
+    expect(hasLockfile()).toEqual([]);
   });
 
   it('removes all lockfiles', async () => {
-    shell.touch('package-lock.json');
-    shell.touch('yarn.lock');
+    shell.touch(['package-lock.json', 'yarn.lock']);
+
     const res = await removeLockfiles();
 
-    expect(hasLockfile()).toEqual([]);
     expect(res).toEqual(['package-lock.json', 'yarn.lock']);
+    expect(hasLockfile()).toEqual([]);
   });
 
   it('does nothing', async () => {
     const res = await removeLockfiles();
 
-    expect(hasLockfile()).toEqual([]);
     expect(res).toEqual([]);
+    expect(hasLockfile()).toEqual([]);
   });
 });
 
 describe('outside CWD', () => {
   it('removes package-lock.json', async () => {
     shell.touch('../package-lock.json');
+
     const res = await removeLockfiles('../');
 
-    expect(hasLockfile('../')).toEqual([]);
     expect(res).toEqual(['package-lock.json']);
+    expect(hasLockfile('../')).toEqual([]);
   });
 
   it('removes yarn.lock', async () => {
     shell.touch('../yarn.lock');
+
     const res = await removeLockfiles('../');
 
-    expect(hasLockfile('../')).toEqual([]);
     expect(res).toEqual(['yarn.lock']);
+    expect(hasLockfile('../')).toEqual([]);
   });
 
   it('removes all lockfiles', async () => {
-    shell.touch('../package-lock.json');
-    shell.touch('../yarn.lock');
+    shell.touch(['../package-lock.json', '../yarn.lock']);
+
     const res = await removeLockfiles('../');
 
-    expect(hasLockfile()).toEqual([]);
     expect(res).toEqual(['package-lock.json', 'yarn.lock']);
+    expect(hasLockfile()).toEqual([]);
   });
 
   it('does nothing', async () => {
     const res = await removeLockfiles('../');
 
-    expect(hasLockfile('../')).toEqual([]);
     expect(res).toEqual([]);
+    expect(hasLockfile('../')).toEqual([]);
   });
 });
