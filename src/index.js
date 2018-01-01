df331d99b19e20da6c1b2e7891cf0511c3845d4f
Lufty Wiranda <lufty.wiranda@gmail.com>
2018-01-01 19:58:29 +0700

    refactor(unstage): extract unstaging to separate function

--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ import del from 'del';
 import execa from 'execa';
 import hasLockfile from 'has-lockfile';
 
+const forceUnstage = async (files, path) => {
+  return execa('git', ['rm', '-f', files.join(' ')], {
+    cwd: path,
+    reject: false,
+  });
+};
+
 const removeLockfiles = async (path = process.cwd()) => {
   const lockfiles = hasLockfile(path);
 
   if (lockfiles.length !== 0) {
-    await execa('git', ['rm', '-f', lockfiles.join(' ')], {
-      cwd: path,
-      reject: false,
-    });
-
+    await forceUnstage(lockfiles, path);
     await del(lockfiles, { cwd: path });
   }
 
