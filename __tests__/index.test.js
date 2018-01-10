4cbd13c73f5d54e36bd61c907b2dade10fee1f40
Lufty Wiranda <lufty.wiranda@gmail.com>
2018-01-11 01:29:20 +0700

    test: improve tests (#55)

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,111 +1,34 @@
 'use strict';
 
-const fs = require('fs-extra');
+const { copySync, existsSync } = require('fs-extra');
+const execa = require('execa');
 
 const removeLockfiles = require('../');
 
-describe('CWD', () => {
-  it('removes package-lock.json', async () => {
-    expect.assertions(2);
-    await fs.copy(
-      `${__dirname}/fixtures/_package-lock.json`,
-      `package-lock.json`
-    );
+it('forces remove lockfiles', async () => {
+  expect.assertions(3);
+  const fixtures = `${__dirname}/fixtures`;
+  copySync(`${fixtures}/_package-lock.json`, `${fixtures}/package-lock.json`);
+  copySync(`${fixtures}/_yarn.lock`, `${fixtures}/yarn.lock`);
+  await execa('git', [
+    'add',
+    `${fixtures}/package-lock.json`,
+    `${fixtures}/yarn.lock`,
+  ]);
 
-    const res = await removeLockfiles();
+  const res = await removeLockfiles(fixtures);
 
-    expect(res).toEqual(['package-lock.json']);
-    expect(fs.pathExistsSync('package-lock.json')).toBe(false);
-  });
-
-  it('removes yarn.lock', async () => {
-    expect.assertions(2);
-    await fs.copy(`${__dirname}/fixtures/_yarn.lock`, `yarn.lock`);
-
-    const res = await removeLockfiles();
-
-    expect(res).toEqual(['yarn.lock']);
-    expect(fs.pathExistsSync('yarn.lock')).toBe(false);
-  });
-
-  it('removes all lockfiles', async () => {
-    expect.assertions(3);
-    await fs.copy(
-      `${__dirname}/fixtures/_package-lock.json`,
-      `package-lock.json`
-    );
-    await fs.copy(`${__dirname}/fixtures/_yarn.lock`, `yarn.lock`);
-
-    const res = await removeLockfiles();
-
-    expect(res).toEqual(['package-lock.json', 'yarn.lock']);
-    expect(fs.pathExistsSync('package-lock.json')).toBe(false);
-    expect(fs.pathExistsSync('yarn.lock')).toBe(false);
-  });
-
-  it('does nothing', async () => {
-    expect.assertions(1);
-
-    const res = await removeLockfiles();
-
-    expect(res).toEqual([]);
-  });
+  const expected = expect.arrayContaining(['yarn.lock', 'package-lock.json']);
+  expect(res).toEqual(expected);
+  expect(existsSync(`${fixtures}/package-lock.json`)).toBe(false);
+  expect(existsSync(`${fixtures}/yarn.lock`)).toBe(false);
 });
 
-describe('outside CWD', () => {
-  it('removes package-lock.json', async () => {
-    expect.assertions(2);
-    await fs.copy(
-      `${__dirname}/fixtures/_package-lock.json`,
-      `${__dirname}/fixtures/package-lock.json`
-    );
+it('returns empty array when no lockfile found', async () => {
+  expect.assertions(3);
+  const res = await removeLockfiles();
 
-    const res = await removeLockfiles(`${__dirname}/fixtures`);
-
-    expect(res).toEqual(['package-lock.json']);
-    expect(fs.pathExistsSync(`${__dirname}/fixtures/package-lock.json`)).toBe(
-      false
-    );
-  });
-
-  it('removes yarn.lock', async () => {
-    expect.assertions(2);
-    await fs.copy(
-      `${__dirname}/fixtures/_yarn.lock`,
-      `${__dirname}/fixtures/yarn.lock`
-    );
-
-    const res = await removeLockfiles(`${__dirname}/fixtures`);
-
-    expect(res).toEqual(['yarn.lock']);
-    expect(fs.pathExistsSync(`${__dirname}/fixtures/yarn.lock`)).toBe(false);
-  });
-
-  it('removes all lockfiles', async () => {
-    expect.assertions(3);
-    await fs.copy(
-      `${__dirname}/fixtures/_package-lock.json`,
-      `${__dirname}/fixtures/package-lock.json`
-    );
-    await fs.copy(
-      `${__dirname}/fixtures/_yarn.lock`,
-      `${__dirname}/fixtures/yarn.lock`
-    );
-
-    const res = await removeLockfiles(`${__dirname}/fixtures`);
-
-    expect(res).toEqual(['package-lock.json', 'yarn.lock']);
-    expect(fs.pathExistsSync(`${__dirname}/fixtures/package-lock.json`)).toBe(
-      false
-    );
-    expect(fs.pathExistsSync(`${__dirname}/fixtures/yarn.lock`)).toBe(false);
-  });
-
-  it('does nothing', async () => {
-    expect.assertions(1);
-
-    const res = await removeLockfiles(`${__dirname}/fixtures`);
-
-    expect(res).toEqual([]);
-  });
+  expect(res).toEqual([]);
+  expect(existsSync('package-lock.json')).toBe(false);
+  expect(existsSync('yarn.lock')).toBe(false);
 });
