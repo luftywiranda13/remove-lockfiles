c6908c5d265677827889085e546b171ee41d42c8
Lufty Wiranda <lufty.wiranda@gmail.com>
2018-01-11 05:57:53 +0700

    perf(API): run `del` only when `git rm` rejected
    previously, `del` always got triggered

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
 'use strict';
 
+const path = require('path');
 const del = require('del');
 const execa = require('execa');
 const hasLockfile = require('has-lockfile');
 
-const remove = (files, cwd) => {
-  return execa('git', ['rm', '-f'].concat(files), {
-    cwd,
-    reject: false,
-  })
-    .then(() => del(files, { cwd }))
-    .then(() => files);
-};
+const remove = (files, cwd) =>
+  execa('git', ['rm', '-f'].concat(files), { cwd })
+    .then(() => files)
+    .catch(() => del(files, { cwd }))
+    .then(deleted => deleted.map(x => path.basename(x)));
 
 const removeLockfiles = ({ cwd = process.cwd(), shrinkwrap = false } = {}) => {
   const lockfiles = shrinkwrap
     ? hasLockfile(cwd)
-    : hasLockfile(cwd).filter(lockfile => lockfile !== 'npm-shrinkwrap.json');
+    : hasLockfile(cwd).filter(x => x !== 'npm-shrinkwrap.json');
 
-  return lockfiles.length === 0
-    ? Promise.resolve(lockfiles)
-    : remove(lockfiles, cwd);
+  return lockfiles.length > 0
+    ? remove(lockfiles, cwd)
+    : Promise.resolve(lockfiles);
 };
 
 module.exports = removeLockfiles;
