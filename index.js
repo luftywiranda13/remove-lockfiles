94489ef401febdab3d76544ad7f529fb46167d9f
Lufty Wiranda <lufty.wiranda@gmail.com>
2018-01-11 01:36:15 +0700

    fix(API): incorrect way of passing args to `execa` (#56)
    Fixes business logic of force unstaging

--- index.js
+++ index.js
@@ -4,21 +4,21 @@ const del = require('del');
 const execa = require('execa');
 const hasLockfile = require('has-lockfile');
 
-const forceGitRemove = (lockfiles, cwd) => {
-  return execa('git', ['rm', '-f', lockfiles.join(' ')], {
+const remove = (files, cwd) => {
+  return execa('git', ['rm', '-f'].concat(files), {
     cwd,
     reject: false,
-  });
+  })
+    .then(() => del(files, { cwd }))
+    .then(() => files);
 };
 
 const removeLockfiles = (cwd = process.cwd()) => {
   const lockfiles = hasLockfile(cwd);
 
-  return lockfiles.length > 0
-    ? forceGitRemove(lockfiles, cwd)
-        .then(() => del(lockfiles, { cwd }))
-        .then(() => lockfiles)
-    : Promise.resolve(lockfiles);
+  return lockfiles.length === 0
+    ? Promise.resolve(lockfiles)
+    : remove(lockfiles, cwd);
 };
 
 module.exports = removeLockfiles;
