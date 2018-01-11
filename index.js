0d330b08c662df43a38f8204a3d22547298136b3
Lufty Wiranda <lufty.wiranda@gmail.com>
2018-01-12 03:38:10 +0700

    refactor(API): move force deletion logic to `force-del`

--- index.js
+++ index.js
@@ -1,24 +1,17 @@
 'use strict';
 
 const path = require('path');
-const del = require('del');
-const execa = require('execa');
+const forceDel = require('force-del');
 const hasLockfile = require('has-lockfile');
-
-const remove = (files, cwd) =>
-  execa('git', ['rm', '-f'].concat(files), { cwd })
-    .then(() => files)
-    .catch(() => del(files, { cwd }))
-    .then(deleted => deleted.map(x => path.basename(x)));
 
 const removeLockfiles = ({ cwd = process.cwd(), shrinkwrap = false } = {}) => {
   const lockfiles = shrinkwrap
     ? hasLockfile(cwd)
     : hasLockfile(cwd).filter(x => x !== 'npm-shrinkwrap.json');
 
-  return lockfiles.length > 0
-    ? remove(lockfiles, cwd)
-    : Promise.resolve(lockfiles);
+  return forceDel(lockfiles, { cwd }).then(paths =>
+    paths.map(x => path.basename(x))
+  );
 };
 
 module.exports = removeLockfiles;
