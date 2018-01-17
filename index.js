067c9905d9830260fea48fc0d9cd0c43148c3eaa
Lufty Wiranda <lufty.wiranda@gmail.com>
2018-01-17 23:37:42 +0700

    refactor(API): adopt changes in `has-lockfile` v4

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const hasLockfile = require('has-lockfile');
 
 const removeLockfiles = ({ cwd = process.cwd(), shrinkwrap = false } = {}) => {
   const lockfiles = shrinkwrap
-    ? hasLockfile(cwd)
-    : hasLockfile(cwd).filter(x => x !== 'npm-shrinkwrap.json');
+    ? hasLockfile.lockfiles(cwd)
+    : hasLockfile.lockfiles(cwd).filter(x => x !== 'npm-shrinkwrap.json');
 
   return forceDel(lockfiles, { cwd });
 };
