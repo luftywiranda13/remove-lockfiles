a707fc9db0d35fc1b94f0879746fd22d45f98519
Lufty Wiranda <lufty.wiranda@gmail.com>
2018-12-19 01:22:23 +0700

    test: resolve problem on appveyor (#83)

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const { join } = require('path');
+const execa = require('execa');
 const fixtures = require('fixturez');
 
 const removeLockfiles = require('..');
@@ -18,6 +19,8 @@ it('defaults to not remove `npm-shrinkwrap.json`', async () => {
   const tempDir = f.copy('lockfiles');
   process.chdir(tempDir);
 
+  await execa('git', ['init'], { cwd: tempDir });
+
   await expect(removeLockfiles()).resolves.not.toEqual([
     join(tempDir, 'npm-shrinkwrap.json'),
   ]);
@@ -28,6 +31,8 @@ it('can be set to remove `npm-shrinkwrap.json`', async () => {
 
   const tempDir = f.copy('lockfiles');
   process.chdir(tempDir);
+
+  await execa('git', ['init'], { cwd: tempDir });
 
   await expect(removeLockfiles({ shrinkwrap: true })).resolves.toEqual([
     join(tempDir, 'package-lock.json'),
